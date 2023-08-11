7355a0a37701c8c3237cc4547a01a4795e222155
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-10 17:42:30 -0700

    My first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  *
  * Description: this code is a statement
  *
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
