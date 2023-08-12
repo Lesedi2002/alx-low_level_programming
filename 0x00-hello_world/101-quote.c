51027574144460a53a673028b2ac2d5e5429c03a
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-12 10:21:20 -0700

    My second commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 /**
  * main - Entry point
  *
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	const char message[] = ("and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
+
+	syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
 	return (1);
 }
