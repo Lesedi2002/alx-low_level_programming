d6fb4ff3fc0f92a229df9a9a1cbaaff7f91dce68
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-11 20:37:15 -0700

    My second commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; 1++)
+		printf("%d", 1);
+	printf('\n');
+	return (0);
+}
