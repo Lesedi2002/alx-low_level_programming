6ec1b13b39fcebfda7c3d4029e0d9b123dd72d97
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-13 02:54:58 -0700

    My first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,7 @@
 
 int main(void)
 {
-	const char message[] = ("and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
-
-	syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
+	write(2, "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
