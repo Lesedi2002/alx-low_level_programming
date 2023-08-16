8e9290c45885139a71018ba4f887378335422c05
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-16 06:15:15 -0700

    My third commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print the first 50 fibonacci nubers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long count, a, b, c;
+
+	a = 0;
+	b = 1;
+	for (count = 0; count < 50; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%lu, k);
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
