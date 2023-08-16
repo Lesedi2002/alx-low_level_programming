1983b9da7c7f4fedbe930c38bfe9bb58b662fdf3
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-16 00:53:08 -0700

    My third commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - Entry point
+ * @a: first number to be added
+ * @b: second number to be added
+ * Return: value of sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
