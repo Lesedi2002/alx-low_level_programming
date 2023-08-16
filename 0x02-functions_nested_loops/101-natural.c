53612287050bb655f32d92beb58dabed295fd345
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-16 05:56:23 -0700

    My third commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print the sun of all multiples of all the multiples of 3 or 5
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	int a, sum;
+
+	for (a = 0; a < 1024; a++)
+		if (a % 3 == 0 || a % == 0)
+			num += a;
+	printf("%d\n", sum);
+	return (0);
+}
