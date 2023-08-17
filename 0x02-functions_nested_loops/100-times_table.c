08ba4fb0e52089c43889ba3cd0fb98a9ae706658
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-16 18:44:05 -0700

    My third commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,39 +10,41 @@ void print_times_table(int n)
 {
 	int dig, mul, num;
 
-	if (!(n > 15 || n < 0))
+	if (n >= 0 && n <= 15)
 	{
 		for (dig = 0; dig <= n; dig++)
 		{
 			for (mul = 0; mul <= n; mul++)
 			{
-				num = (dig * mul);
-				if (mul != 0)
+				num = dig * mul;
+				if (mul == 0)
+				{
+					_putchar(num + '0');
+				}
+				else if (num < 10 && mul != 0)
 				{
 					_putchar(',');
-					_putchar(' ');
-				}
-				if (num < 10 && mul != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((num % 10) + '0');
-
+					_putchar('');
+					_putchar('');
+					_putchar('');
+					_putchar(num + '0');
 				}
 				else if (num >= 10 && num < 100)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((num / 10) + '0');
-					_putchar((num & 10) + '0');
+					_putchar((num % 10) + '0');
 				}
-				else if (num >= 100 && mul != 0)
+				else if (num >= 100)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar((num / 100) + '0');
-					_putchar((num / 10) % 10 + '0');
+					_putchar(((num / 10) % 10) + '0');
 					_putchar((num % 10) + '0');
 				}
-				else
-					_putchar((num % 10) + '0');
 			}
 			_putchar('\n');
 		}
