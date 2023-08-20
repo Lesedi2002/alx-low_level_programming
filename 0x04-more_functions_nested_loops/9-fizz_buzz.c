48f249b93ee7c2be299e247ea7208b98749efbe2
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-19 17:44:16 -0700

    More functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,24 +13,18 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0 && a % 5 != 0)
-		{
-			printf("Fizz");
-		} else if (a % 5 == 0 && a % 3 != 0)
-		{
-			printf("Buzz");
-		} else if (a % 3 == 0 && a % 5 == 0)
-		{
+		if (a % 15 == 0)
 			printf("FizzBuzz");
-		} else if (a == 1)
-		{
+		else if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else
 			printf("%d", a);
-		} else
-		{
-			printf(" %d", a);
-		}
+
+		if (a < 100)
+			printf(" ");
 	}
 	printf("\n");
-
 	return (0);
 }
