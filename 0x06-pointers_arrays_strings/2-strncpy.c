62a3423ff7547fba5375114b1a0ed69e0a8d2790
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-23 13:30:22 -0700

    More pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,8 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int a;
 
 	for (a = 0; a < n && src[a] != '\0'; a++)
-		*(dest +  1) = *(src + 1);
+	{
+		dest[a] = src[a];
+	}
 	for (; a < n; a++)
-		*(dest + a) = '\0';
+	{
+		dest[a] = '\0';
+	}
 	return (dest);
 }
