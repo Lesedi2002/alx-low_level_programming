c926981a18fc1ac3023cded1bd735aa7cec8c596
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-24 04:59:26 -0700

    More pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,3 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 /**
+ * rot13 - function that encodes a string using rot13
+ * @str: string input
  *
+ * Return: encoded string
+ */
+
+char *rot13(char *str)
+{
+
