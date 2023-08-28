a122bfd8734ba97ccd2c81dcd393cab0998106f6
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-28 08:43:59 -0700

    Even more pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - function that locates substring
+ * @haystack: source string
+ * @needle: searching string
+ *
+ * Return: return a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (haystack = 0; *haystack != '\0'; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+		if (*b == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
