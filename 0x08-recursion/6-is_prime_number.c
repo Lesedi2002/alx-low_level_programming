8793f36057cbb1582b3e0858994ea758c0f67902
Lesedi2002 <lesedinyokong@gmail.com>
2023-08-30 14:28:02 -0700

    Recursion task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
+ * prime_number - function that returns 1 if prime number else 0
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 or 1
+ */
+
+int prime_number(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime_number(a, b + 1));
+}
+
+/**
  * is_prime_number - function that returns 1 if prime number else 0
  * @n: integer input
  *
@@ -8,14 +25,7 @@
 
 int is_prime_number(int n)
 {
-	if (n <= 0)
+	if (n <= 1)
 		return (0);
-	if (n == 2)
-		return (1);
-	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
-		return (0);
-	else if (n % 9 == 0 || n % 7 == 0)
-		return (0);
-	else
-		return (1);
+	return (prime_number(n, 2));
 }
