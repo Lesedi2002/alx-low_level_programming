cb03521546c0d6f72a497c066bf6cb8571e82753
Lesedi2002 <lesedinyokong@gmail.com>
2023-09-22 14:22:05 -0700

    lists task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,39 +1,46 @@
 #include "lists.h"
-
 /**
- * add_node - adds a new node at the beginning of a list
- *@head: pointer to the head of the list
- *@str: string to be added
- *
- * Return: returns the address to the new element or NULL
- * if failed
-*/
-
-list_t *add_node(list_t **head, const char *str)
+ * len - Calculates the length of a string
+ * @str: Constant string
+ * Return: The length of the string
+ */
+int len(const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new;
+	int count;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+	if (str == NULL)
+		return (0);
+	for (count = 0; str[count] != '\0'; count++)
+		;
+	return (count);
+}
+/**
+ * add_node_end - Function that adds a new node at the end of a list_t list.
+ *
+ * @head: This is the input linked list
+ * @str: This is the string take in my linked list.
+ *
+ * Return: the address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+	list_t *last = *head;
 
-	dup = strdup(str);
-	if (dup == NULL)
+	if (new_node == NULL || str == NULL)
 	{
-		free(new);
+		free(new node);
 		return (NULL);
 	}
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
-
+	new_node->str = strdup(str);
+	new_node->len = len(str);
+	new_node->next = NULL;
+	{
+		*head = new_node;
+		return (*head);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return (*head);
 }
